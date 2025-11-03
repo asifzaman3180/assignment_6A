3699b19f898c178b9800efa007a1ea5fffc6646a
Rinza129 <s2312276129@ru.ac.bd>
2025-11-03 12:33:54 +0600

    Update Code Style for Better Consistency

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -16,7 +16,11 @@ public class EmployeeManager {
                 for (String emp : e) {
                     System.out.println(emp);
                 }
-            } catch (Exception e) {}
+            } 
+            catch (Exception e) 
+            {
+                
+            }
             System.out.println("Data Loaded.");
         } else if (args[0].equals("s")) {
             System.out.println("Loading data ...");
