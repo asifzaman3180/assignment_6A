b9f22df1264d044cb2ac3e070715db04ea069376
Abdul-Karim-32 <s2410576143@ru.ac.bd>
2025-11-03 12:32:25 +0600

    task2

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class EmployeeManager {
     public static void main(String[] args) {
         // Check arguments
+        if(args.length == 0) {
+          System.out.println("No arguments");
+          return;
+        }
         if (args[0].equals("l")) {
             System.out.println("Loading data ...");
             try {
